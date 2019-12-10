f3c2119c19b48d3443e6555041c34df6bf7420f9
Amari <sewell.amari@yahoo.com>
2019-12-09 20:06:47 -0500

    Merge branch 'master' of C:\Users\Atari\Desktop\os-final-project with conflicts.

---
+++ cmake-build-debug/CMakeFiles/Memory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Memory.dir/main.c.obj"
+  "Memory.exe"
+  "Memory.exe.manifest"
+  "Memory.lib"
+  "Memory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
